e891b5b847d8f7c744720709d38166d75613171f
AndyDeany <oneandydean@hotmail.com>
2017-09-08 19:58:45 +0100

    Add deploy.sh file for deploying to heroku

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+if [ $# == 0 ]
+then
+    echo "You must provide an environment"
+    exit 1
+fi
+
+
+site=daviddean
+environment=${1,,}
+
+if [ $environment != "prod" ]
+then
+    site=$environment-$site
+fi
+
+heroku git:remote -a $site
+git push heroku master
